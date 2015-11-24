9cc30aa5e511636f8afd68b768490b983fd0b1e1
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2015-11-23 19:31:46 -0500

    test commit

--- src/main/java/com/example/NotFoundException.java
+++ src/main/java/com/example/NotFoundException.java
@@ -4,6 +4,6 @@ public class NotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 6285867531715388561L;
 
 	public NotFoundException(String id) {
-        super(String.format("No entry found with id: <%s>", id));
+        super(String.format("No1 entry found with id: <%s>", id));
     }
 }
